c413501515da7926fda2a64b13278d4a587c1dec
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2023-11-20 07:54:23 +0400

    initial

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# kubernetes_exercises
-Exercises and the validation scripts to determine if they are completed and successful
+# Kubernetes Validation Tool
+
+This tool provides a set of functions to validate various aspects of a Kubernetes cluster, including pod connectivity, URL accessibility from within and outside the cluster, and user permissions.
+
+## Features
+
+- **Pod Connectivity Check**: Validates if one pod can connect to another within the cluster.
+- **Internal URL Accessibility Check**: Validates if a URL is accessible from a specific pod within the cluster.
+- **External URL Accessibility Check**: Validates if a URL is accessible from outside the Kubernetes cluster.
+- **User Permission Check**: Checks if a user has specific permissions in the Kubernetes cluster.
+- **Controller Status Check**: Determines if a specific Kubernetes controller is enabled.
+- **API Version Availability Check**: Checks if a specific version of an API is available in the Kubernetes cluster.
+
+## Requirements
+
+- Python 3.x
+- `kubectl` installed and configured to communicate with your Kubernetes cluster.
+- Python `requests` library for HTTP requests (for external URL checks).
+
+## Installation
+
+1. Ensure Python 3.x is installed on your system.
+2. Install the required Python packages:
+   
+   ```bash
+   pip install requests
+   ```
+
+3. Clone the repository or download the script files to your local machine.
+
+## Usage
+
+To validate a specific Kubernetes exercise, run the script with the exercise name as an argument. The script will perform all necessary checks and provide you with the results.
+
+Example command:
+
+```bash
+python ckad_validate.py exercise_name
+```
+
+## Contributing
+
+Contributions to this tool are welcome. Please ensure that any pull requests or issues adhere to the existing coding style and functionality.
+
---
+++ ckad_validate.py
@@ -0,0 +1,188 @@
+import subprocess
+import sys
+import logging
+import requests
+import re
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def is_kubectl_available():
+    """Check if kubectl is available."""
+    try:
+        subprocess.run(["kubectl", "version", "--client"], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+def run_command(command):
+    """Run a shell command and return its output."""
+    try:
+        result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+        return result.stdout.strip()
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Command failed: {e.stderr}")
+        sys.exit(1)
+
+def validate_resource(resource_type, resource_name):
+    """Validate if a specific Kubernetes resource exists."""
+    logging.info(f"Validating {resource_type} {resource_name}...")
+    run_command(f"kubectl get {resource_type} {resource_name}")
+
+def validate_pod_connectivity(pod_name, target_pod_name, target_port):
+    """Validate if one pod can connect to another on a specific port."""
+    logging.info(f"Checking connectivity from {pod_name} to {target_pod_name} on port {target_port}...")
+
+    # Define the command to check connectivity using netcat (nc)
+    command = f"kubectl exec {pod_name} -- nc -zv {target_pod_name} {target_port}"
+
+    try:
+        output = run_command(command)
+        if "succeeded" in output:
+            logging.info(f"Connectivity from {pod_name} to {target_pod_name} on port {target_port} is successful.")
+        else:
+            logging.error(f"Connectivity test failed from {pod_name} to {target_pod_name} on port {target_port}.")
+            logging.error(f"Command output: {output}")
+            sys.exit(1)
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Failed to execute connectivity test: {e}")
+        sys.exit(1)
+
+def validate_url_accessibility(pod_name, url):
+    """Validate if a URL is accessible from a specific pod."""
+    logging.info(f"Checking URL accessibility from {pod_name} to {url}...")
+
+    command = f"kubectl exec {pod_name} -- curl -s -o /dev/null -w '%{{http_code}}' {url}"
+
+    try:
+        http_status = run_command(command)
+        if http_status == "200":
+            logging.info(f"URL {url} is accessible from pod {pod_name}.")
+        else:
+            logging.error(f"URL {url} is not accessible from pod {pod_name}. HTTP status code: {http_status}")
+            sys.exit(1)
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Failed to execute URL accessibility test: {e}")
+        sys.exit(1)
+
+def validate_external_url_accessibility(url):
+    """Validate if a URL is accessible from outside the Kubernetes cluster."""
+    logging.info(f"Checking external URL accessibility for {url}...")
+
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            logging.info(f"URL {url} is externally accessible.")
+        else:
+            logging.error(f"URL {url} is not externally accessible. HTTP status code: {response.status_code}")
+            return False
+    except requests.RequestException as e:
+        logging.error(f"Failed to execute external URL accessibility test: {e}")
+        return False
+    return True
+
+def check_user_permission(verb, resource, namespace="default", username=None):
+    """Check if a specified user has permissions to perform a given action on a resource."""
+    user_info = f"user '{username}'" if username else "the current user"
+    logging.info(f"Checking if {user_info} has permission to '{verb}' on '{resource}' in '{namespace}' namespace...")
+
+    command = f"kubectl auth can-i {verb} {resource} --namespace {namespace}"
+    if username:
+        command += f" --as {username}"
+
+    try:
+        output = run_command(command)
+        if output.strip() == "yes":
+            logging.info(f"{user_info} has permission to '{verb}' on '{resource}' in '{namespace}' namespace.")
+            return True
+        else:
+            logging.warning(f"{user_info} does not have permission to '{verb}' on '{resource}' in '{namespace}' namespace.")
+            return False
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Failed to execute permission check: {e}")
+        sys.exit(1)
+
+def check_controller_enabled(controller_name):
+    """Check if a specific Kubernetes controller is enabled."""
+    logging.info(f"Checking if the controller '{controller_name}' is enabled...")
+
+    try:
+        # This command might vary depending on your cluster setup and the controller
+        output = run_command("kubectl get deployment -n kube-system")
+        if controller_name in output:
+            logging.info(f"Controller '{controller_name}' is enabled.")
+            return True
+        else:
+            logging.warning(f"Controller '{controller_name}' is not enabled or not found.")
+            return False
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Failed to check controller status: {e}")
+        sys.exit(1)
+
+def check_api_version_available(api_version):
+    """Check if a specific version of an API is available in the Kubernetes cluster."""
+    logging.info(f"Checking if API version '{api_version}' is available...")
+
+    try:
+        output = run_command("kubectl api-versions")
+        if api_version in output.splitlines():
+            logging.info(f"API version '{api_version}' is available.")
+            return True
+        else:
+            logging.warning(f"API version '{api_version}' is not available.")
+            return False
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Failed to check API version availability: {e}")
+        sys.exit(1)
+
+def validate_pod_details_from_file(file_path):
+    """Validate pod details from a text file."""
+    logging.info(f"Validating pod details from {file_path}...")
+
+    try:
+        with open(file_path, 'r') as file:
+            for line_number, line in enumerate(file, start=1):
+                line = line.strip()
+
+                if not line:
+                    continue
+
+                if re.match(r'^([^/]+/)+[^/]+$', line):
+                    logging.info(f"Line {line_number} is valid: {line}")
+                else:
+                    logging.error(f"Line {line_number} is invalid: {line}")
+                    return False
+        return True
+    except FileNotFoundError:
+        logging.error(f"File not found: {file_path}")
+        return False
+    except IOError as e:
+        logging.error(f"Error reading file {file_path}: {e}")
+        return False
+
+def execute_validation(exercise_number):
+    """Execute validation based on exercise number using a dynamic approach."""
+    try:
+        function_name = f"validate_exercise_{exercise_number}"
+        validation_function = getattr(sys.modules[__name__], function_name)
+        validation_function()
+    except AttributeError:
+        logging.error(f"No validation function found for exercise number {exercise_number}")
+        sys.exit(1)
+    except Exception as e:
+        logging.error(f"An error occurred during validation: {e}")
+        sys.exit(1)
+
+def main():
+    if not is_kubectl_available():
+        logging.error("kubectl is not available. Please install it and ensure it's in your PATH.")
+        sys.exit(1)
+
+    try:
+        exercise_number = int(input("Enter the exercise number to validate: "))
+        execute_validation(exercise_number)
+    except ValueError:
+        logging.error("Please enter a valid number.")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
